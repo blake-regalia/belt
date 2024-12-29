b5ca04bd4adc3aa42921e8e9f051b6db6156a326
Blake Regalia <blake.regalia@gmail.com>
2024-12-29 00:55:47 -0800

    chore: fix dev dep

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
 import elite from '@blake.regalia/eslint-config-elite';
-debugger;
+
 export default [
 	...elite,
 	{
 		languageOptions: {
 			 ecmaVersion: 2022,
 			 sourceType: 'module',
-			//  globals: {
-			// 	  ...globals.browser,
-			// 	  ...globals.node,
-			// 	  chrome: 'readonly',
-			//  },
-			//  parser: tseslint_parser,
+
 			 parserOptions: {
 				  tsconfigRootDir: import.meta.dirname,
 				  project: 'tsconfig.json',
